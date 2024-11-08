c7271c18496b3ddaa7e5b767a3b18ae4ee78cec3
mmdimran333@gmail.com <mmdimran333@gmail.com>
2024-11-08 19:30:14 +0530

    we created teacher api

---
+++ src/main/java/com/rtt/teacher/TeacherI.java
@@ -0,0 +1,7 @@
+package com.rtt.teacher;
+
+import com.rtt.teacher.SuccessTeacherResponse;
+
+public interface TeacherI {
+    public SuccessTeacherResponse createTeacher(TeacherRequest teacherRequest );
+}
