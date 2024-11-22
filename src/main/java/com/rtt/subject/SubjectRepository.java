f4de3142ce9eaddc59faeeac43d8da4d17d5d3d1
mmdimran333@gmail.com <mmdimran333@gmail.com>
2024-11-22 20:05:13 +0530

    Subject API Commited

--- src/main/java/com/rtt/subject/SubjectRepository.java
+++ src/main/java/com/rtt/subject/SubjectRepository.java
@@ -1,4 +1,17 @@
 package com.rtt.subject;
 
-public class SubjectRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+
+   @Query(value="select subject_Name,subject_Id from Subject_master" , nativeQuery = true)
+   List<Object[]> getSubjectNameList();
+
+
+
 }
