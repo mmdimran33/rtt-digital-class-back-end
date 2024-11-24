4fe246eb34b620dd71606061bde2747a26483e5c
PRAMOD KUMAR <43pramod@gmail.com>
2024-11-24 17:47:41 +0530

    Teacher details

--- src/main/java/com/rtt/teacher/TeacherServiceImpl.java
+++ src/main/java/com/rtt/teacher/TeacherServiceImpl.java
@@ -3,43 +3,58 @@ package com.rtt.teacher;
 import com.rtt.common.SuccessRegistrationResponse;
 import com.rtt.constants.RegistrationResponseConstants;
 import com.rtt.exception.RegistrationException;
+import com.rtt.subject.Subject;
+import com.rtt.subject.SubjectRepository;
 import com.rtt.teacher.SuccessTeacherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class TeacherServiceImpl implements TeacherI{
 
     @Autowired
-    private TeacherRepository repository;
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository; // Assume this repository exists for Subject
+
+
 
     @Override
     public SuccessTeacherResponse createTeacher(TeacherRequest teacherRequest) {
 
         try {
+            // Fetch subjects from the database
+            Set<Subject> subjects = new HashSet<>( subjectRepository.findAllById(teacherRequest.getSubjectId()));
+
             // Build the Teacher entity from the request
             var teacher = Teacher.builder()
                     .teacherName(teacherRequest.getTeacherName())
-                    .Subject(teacherRequest.getSubject())
                     .phoneNo(teacherRequest.getPhoneNo())
-                    .AadharNo(teacherRequest.getAadharNo())
+                    .aadharNo(teacherRequest.getAadharNo())
                     .salary(teacherRequest.getSalary())
                     .mailId(teacherRequest.getMailId())
                     .teacherQualification(teacherRequest.getTeacherQualification())
-                    //.teacherPhoto(teacherRequest.getTeacherPhoto())
+                    .subjects(subjects) // Associate subjects with teacher
                     .build();
 
             // Save the teacher entity
-            Teacher savedTeacher = repository.save(teacher);
+            Teacher savedTeacher = teacherRepository.save(teacher);
 
             // Check if the teacher ID was generated and return success response
             if (savedTeacher.getTeacherId() != null) {
-                return SuccessTeacherResponse.builder().responseCode(RegistrationResponseConstants.REGISTRATION_RESPONSE_SUCCESS_CODE)
-                        .responseDescription(RegistrationResponseConstants.REGISTRATION_RESPONSE_SUCCESS_DESCTIPTION).build();
+                return SuccessTeacherResponse.builder()
+                        .responseCode(RegistrationResponseConstants.REGISTRATION_RESPONSE_SUCCESS_CODE)
+                        .responseDescription(RegistrationResponseConstants.REGISTRATION_RESPONSE_SUCCESS_DESCTIPTION)
+                        .build();
             }
 
-        }catch (Exception e){
-            throw new RegistrationException(RegistrationResponseConstants.REGISTRATION_RESPONSE_FAILURE_CODE,
+        } catch (Exception e) {
+            throw new RegistrationException(
+                    RegistrationResponseConstants.REGISTRATION_RESPONSE_FAILURE_CODE,
                     RegistrationResponseConstants.REGISTRATION_RESPONSE_FAILURE_DESCTIPTION + e.getMessage());
         }
         return null;
