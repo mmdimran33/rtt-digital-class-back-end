d55499cef3b97af66d623afba10252f90b9e24e0
mmdimran333@gmail.com <mmdimran333@gmail.com>
2024-11-25 10:44:06 +0530

    Committed

--- src/main/java/com/rtt/teacher/TeacherServiceImpl.java
+++ src/main/java/com/rtt/teacher/TeacherServiceImpl.java
@@ -3,6 +3,7 @@ package com.rtt.teacher;
 import com.rtt.common.SuccessRegistrationResponse;
 import com.rtt.constants.RegistrationResponseConstants;
 import com.rtt.exception.RegistrationException;
+import com.rtt.subject.Subject;
 import com.rtt.teacher.SuccessTeacherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,7 @@ public class TeacherServiceImpl implements TeacherI{
                     .build();
 
             // Save the teacher entity
+//            var subject = Subject.builder().subjectId(teacher.getTeacherId())
             Teacher savedTeacher = repository.save(teacher);
 
             // Check if the teacher ID was generated and return success response
