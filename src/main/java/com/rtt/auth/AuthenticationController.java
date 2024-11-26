7284e9022dac8dfbb447a522d6e5e6a47cc5d850
fjee97 <you@example.com>
2024-11-26 19:56:32 +0530

    added @CrossOrigin in every controller

--- src/main/java/com/rtt/auth/AuthenticationController.java
+++ src/main/java/com/rtt/auth/AuthenticationController.java
@@ -10,14 +10,12 @@ import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
