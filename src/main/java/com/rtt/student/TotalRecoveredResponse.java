1ae71bf776fa2bb905d08e85aa11af47b14fddc9
mmdimran333@gmail.com <mmdimran333@gmail.com>
2024-11-30 20:00:02 +0530

    we created total recovered service response

---
+++ src/main/java/com/rtt/student/TotalRecoveredResponse.java
@@ -0,0 +1,16 @@
+package com.rtt.student;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TotalRecoveredResponse {
+   // private float total_recovered_amount_of_students;
+    private static final long serialVersionUID = 1L;
+   @JsonProperty("total_recovered_amount_of _the_students")
+   private Float paidAmount;
+}
